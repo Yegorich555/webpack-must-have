fa397352e676826be7aedb39286df83bf466b73f
Maxim-front <mkorzhenevskiy2000@mail.ru>
2021-10-28 15:38:19 +0300

    add footer

--- src/components/routes/routes.tsx
+++ src/components/routes/routes.tsx
@@ -1,9 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
+import Footer from "../footer/footer";
 import Header from "../header/header";
+import Routecomponents from "../route-components/route-components";
 
 const Routes = (): JSX.Element => (
   <BrowserRouter>
     <Header />
+    <Routecomponents />
+    <Footer />
   </BrowserRouter>
 );
 
