c88275413c78dfaf5999edcbd41d2132d5047254
Yegor G <yegorich_555@mail.ru>
2021-11-23 11:12:45 +0300

    Linters. MDlint. Update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,5 +5,5 @@ module.exports = {
   "*.{js,ts,json}": ["eslint --fix --cache", "prettier --write"],
   // optional: disable prettier for html files (via removing next line)
   "*.{html}": ["prettier --write"],
-  "*.{md}": ["markdownlint --fix **/*.md"],
+  "*.{md}": ["markdownlint --fix **/*.md --ignore node_modules"],
 };
