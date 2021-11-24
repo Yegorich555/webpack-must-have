b582abc5d211494b055cc54c1b3964abfc41a0a7
PrKsushka <ksushkaprot@gmail.com>
2021-11-25 00:18:41 +0300

    initial commit_add app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import React, { Component } from "react";
 import Header from "./components/header/header";
 import Home from "./pages/home/home";
 import Product from "./pages/product/product";
 import About from "./pages/about/about";
 import Footer from "./components/footer/footer";
 
-const App = function () {
-  return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/product" component={Product} />
-          <Route exact path="/about" component={About} />
-          <Route path="/signIn" />
-          <Route path="/signUp" />
-          <Redirect to="/home" />
-        </Switch>
-        <Footer />
-      </Router>
-    </div>
-  );
+type AppProps = Record<string, never>;
+type AppState = {
+  err: boolean;
 };
+interface Error {
+  stack?: string;
+}
+export default class App extends Component<AppProps, AppState> {
 
-export default App;
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      err: false,
+    };
+  }
+
+  // @ts-ignore
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    alert(error);
+    console.error(error);
+  }
+
+  // @ts-ignore
+  static getDerivedStateFromError(error: Error) {
+    return { err: true };
+  }
+
+  render() {
+    if (!this.state.err) {
+      return (
+        <div className="App">
+          <Router>
+            <Header />
+            <Switch>
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/product" component={Product} />
+              <Route exact path="/product/:category" />
+              <Route exact path="/about" component={About} />
+              <Route path="/signIn" />
+              <Route path="/signUp" />
+              <Redirect to="/home" />
+            </Switch>
+            <Footer />
+          </Router>
+        </div>
+      );
+    }
+
+    return <div>oops we have some problems</div>;
+  }
+}
