a304ce6f8d41887dae6c8042d1ac1c9966e5ec91
PrKsushka <ksushkaprot@gmail.com>
2021-11-25 15:29:33 +0300

    enter value into input and get res

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,32 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 
+type inputElems = {
+  name: string;
+  value: unknown;
+};
+interface PostResult {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 const Home: React.FunctionComponent = function () {
   const [input, setInput] = useState({
     search: "",
   });
+  const [postFound, setPostFound] = useState([]);
 
-  function handleChange(e: { target: { name: string; value: unknown } }) {
+  function handleChange(e: { target: inputElems }) {
     const { name, value } = e.target;
     setInput((prevState) => ({
       ...prevState,
       [name]: value,
     }));
   }
-
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+  const searchByName = async (query: string): Promise<PostResult> => {
+    const result = await fetch(`https://jsonplaceholder.typicode.com/posts?title=${query}`);
+    // eslint-disable-next-line no-return-await
+    return await result.json();
+  };
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((data) => {
-        console.log(data.json());
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
+    (async () => {
+      const query = input.search;
+      if (query) {
+        const response = await searchByName(query);
+        setPostFound((prevState) => ({
+          ...prevState,
+          response,
+        }));
+      }
+    })();
+  }, [input.search]);
+  console.log(postFound);
   return (
     <div>
       <div>
-        <input type="text" name="search" value={input.search} onChange={handleChange} />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="search" value={input.search} onChange={handleChange} />
+        </form>
       </div>
       <div />
       <div />
