43b22bf14de2e4f51dae140b447724cb41a693ab
PrKsushka <ksushkaprot@gmail.com>
2021-11-25 13:27:37 +0300

    first changes for home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home: React.FunctionComponent = function () {
-  return <div>This is home page</div>;
+  const [input, setInput] = useState({
+    search: "",
+  });
+
+  function handleChange(e: { target: { name: string; value: unknown } }) {
+    const { name, value } = e.target;
+    setInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => {
+        console.log(data.json());
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <input type="text" name="search" value={input.search} onChange={handleChange} />
+      </div>
+      <div />
+      <div />
+    </div>
+  );
 };
 export default Home;
