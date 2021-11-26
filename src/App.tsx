46c37dfdad594d4ffe44f87a43a404d6187d8f64
PrKsushka <ksushkaprot@gmail.com>
2021-11-26 15:08:28 +0300

    create new changes

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,20 @@ import ErrorBoundary from "./components/errorBoundary/errorBoundary";
 class App extends Component<unknown, unknown> {
   render() {
     return (
-      <ErrorBoundary>
-        <Router>
-          <Layout>
-            <Switch>
-              <Route exact path={links.home} component={Home} />
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path={links.home} component={Home} />
+            <ErrorBoundary>
               <Route exact path={links.product} component={Product} />
               <Route exact path={links.about} component={About} />
               <Route path={links.signIn} />
               <Route path={links.signUp} />
-              <Redirect to={links.home} />
-            </Switch>
-          </Layout>
-        </Router>
-      </ErrorBoundary>
+            </ErrorBoundary>
+            <Redirect to={links.home} />
+          </Switch>
+        </Layout>
+      </Router>
     );
   }
 }
