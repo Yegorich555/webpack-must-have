210b3a7ab74eb3a19870393824d5089ec9afa879
Maxim-front <mkorzhenevskiy2000@mail.ru>
2021-12-03 14:19:07 +0300

    branch for other PC

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,19 +1,19 @@
 import "react-toastify/dist/ReactToastify.css";
 
-export const getApiResourse = async(url, data = {}) => {
+export const getApiResourse = async (url, data = {}) => {
   const res = await fetch(url, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *client
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-    })
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *client
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
     .then((response) => {
       if (response.status >= 200 && response.status < 300) {
         return response;
@@ -31,7 +31,7 @@ export const getApiResourse = async(url, data = {}) => {
   return result;
 };
 
-export const getApiCardResourse = async(url) => {
+export const getApiCardResourse = async (url) => {
   try {
     const res = await fetch(url);
     if (!res.ok) {
