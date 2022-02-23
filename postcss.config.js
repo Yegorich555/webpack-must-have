a7ce043b4218699d5f066c94e8d4cbf24d0a6965
Yegor G <yegorich_555@mail.ru>
2022-02-23 11:38:17 +0300

    Postcss. Add auto-fix for hover issue on IOS

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies, global-require */
 module.exports = {
   // browser target config see in .browserlistsrc
   parser: "postcss-scss",
-  plugins: {
-    autoprefixer: {}, // it adds vendor prefixes ::placeholder => ::-webkit-input-placeholder, ::-moz-placeholder etc. https://github.com/postcss/autoprefixer
-    "postcss-normalize": {
-      // it adds normalize.css and sanitize.css. https://github.com/csstools/postcss-normalize
-      // forceImport: true, //it adds twice
-      allowDuplicates: false,
-    },
-  },
+  plugins: [
+    require("postcss-hover-media-feature"), // it fixes twice-click issue on buttons with hover effect on IOS: https://css-tricks.com/annoying-mobile-double-tap-link-issue/
+    require("autoprefixer"), // it adds vendor prefixes ::placeholder => ::-webkit-input-placeholder, ::-moz-placeholder etc. https://github.com/postcss/autoprefixer
+    // it adds normalize.css and sanitize.css. https://github.com/csstools/postcss-normalize
+    // forceImport: true, //it adds twice
+    require("postcss-normalize")({ allowDuplicates: false }),
+  ],
 };
