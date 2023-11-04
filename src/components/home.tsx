c3a37c24449fa336eef2e184cca5540aa33aa50c
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-11-04 19:18:13 +0200

    Routing

---
+++ src/components/home.tsx
@@ -0,0 +1,9 @@
+import "./home.scss";
+
+export default function Home() {
+  return (
+    <div className="home__box">
+      <h1>Home</h1>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import "./styles/main.scss";
 // watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
-import imgSmall from "images/testSmall.png"; // start-path is 'images' because we have an alias 'images' in webpack.common.js
-import imgCamera from "images/camera.svg";
+// import imgSmall from "images/testSmall.png"; // start-path is 'images' because we have an alias 'images' in webpack.common.js
+// import imgCamera from "images/camera.svg";
 import { Component, StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { WUPFormElement, WUPTextControl } from "web-ui-pack";
-import style from "./styles/main.module.css";
-import someTypeScript from "./someTypeScript";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./components/home";
+import Products from "./components/products";
+import About from "./components/about";
+import Footer from "./components/footer";
+import routes from "./helpers/constans";
 
-!(WUPFormElement && WUPTextControl) && console.warn("err");
+// import style from "./styles/main.module.css";
+// import someTypeScript from "./someTypeScript";
 
 interface AppProps {
   nothing: boolean;
@@ -23,9 +28,9 @@ class AppContainer extends Component<AppProps, AppState> {
 
   constructor(props: AppProps) {
     super(props);
-    this.state = {
-      title: someTypeScript("Test-block for css-modules"),
-    };
+    // this.state = {
+    //   title: someTypeScript("Test-block for css-modules"),
+    // };
     // test class-dead-code
     const goExlcude = true;
     if (!goExlcude) {
@@ -36,25 +41,18 @@ class AppContainer extends Component<AppProps, AppState> {
   render() {
     return (
       <StrictMode>
-        <div className="test-block">
-          <h2 className={style.mainTitle}>{this.state.title}</h2>
-        </div>
-        <div className={["test-block", style.background].join(" ")}>
-          <h2>Test-block for assets-module (previous url-loader)</h2>
-          <img src={imgSmall} alt="smallImage" />
-        </div>
-        {/*  or it can be
-          <img src='/src/images/testSmall.png' alt="smallImage"></img>
-        */}
-        <div className={["test-block", style.svgBackground].join(" ")}>
-          <h2>Test-block for assets-module (svg-url-loader)</h2>
-          <img src={imgCamera} alt="small_SVG_Image" />
-        </div>
-
-        <wup-form class={style.form}>
-          <wup-text w-name="TextControl" />
-          <button type="submit">Submit</button>
-        </wup-form>
+        <BrowserRouter>
+          <Header />
+          <main>
+            <Routes>
+              <Route path={routes.home} element={<Home />} />
+              <Route path={routes.products} element={<Products />} />
+              <Route path={routes.about} element={<About />} />
+              <Route path={routes.redirect} element={<Home />} />
+            </Routes>
+          </main>
+          <Footer />
+        </BrowserRouter>
       </StrictMode>
     );
   }
