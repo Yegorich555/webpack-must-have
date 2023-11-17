b847b88fd57f2ed67c9ca5cbd4165cee0f1585bd
Yegorich555 <yegorich555@gmail.com>
2023-11-17 18:16:30 +0200

    Packages. Remove useless babel-jsx

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   presets: [
-    "@babel/preset-react", // optional: react: this resolves react-files (jsx, tsx)
-    // doesn't required because ts-loader in use // "@babel/preset-typescript", // allows  to use TypeScript
     "@babel/preset-env", // compiles your js according with .browserslistrc
   ],
-  plugins: [
-    "jsx-classnames-advanced", // optional-react: this resolves className={object}
-  ],
+  plugins: [],
 };
